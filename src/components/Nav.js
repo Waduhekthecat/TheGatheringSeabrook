4e84b977c3dca980ce95b3113a5e585700175ff0
Waduhekthecat <Waduhekthecat@yahoo.com>
2022-07-19 22:17:47 -0500

    menu icon and dialog for small screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,17 @@ import Logo from '../imgs/logo-no-bg.png';
 import Background from '../imgs/wood-texture.jpg';
 import Logo1 from '../imgs/LogoOg.png';
 import { Link } from 'react-router-dom';
+import Dialog from '@mui/material/Dialog';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+  import Slide from '@mui/material/Slide';
+  import menu from '../imgs/menu.png';
+
+  const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 
 function Nav() {
     const navStyle = {
@@ -12,18 +23,22 @@ function Nav() {
         listStyle: 'none',
         textDecoration: 'none'
     };
-    const [showModal, setShowModal] = useState(false);
-    const [isHide, setIsHide] = useState(false);
-    const toggleModal = () =>
-        setShowModal(!showModal);
-    const _onHideMenu = () => {
-        setIsHide(!isHide)
+    const [open, setOpen] = useState(false);
+
+    const handleDialog = () => {
+      !open ? setOpen(true) : setOpen(false);
     };
+    console.log('mobile menu opened: ', {open});
+
 
     return (
-        <div style={{background:'white', height:'85px', width:'100vw', position:'fixed', zIndex:'350'}}>
+        <>
+        <Wrapper>
+        <Menu style={{cursor:'pointer'}}onClick={handleDialog}/>
+        <NavLogo2 src={Logo} alt="The Gathering" />
             <Navbar>
-                <ul className="nav-links">
+               
+                <ul class='nav-links'>
                     <NavLink>
                         <Link style={navStyle} to='/'>
                             <li style={{ color: 'gray' }}>Home</li>
@@ -51,13 +66,85 @@ function Nav() {
                     </NavLink>
                 </ul>
             </Navbar>
-        </div>
+        </Wrapper>
+
+{/* dialog menu */}
+<Dialog
+fullScreen
+open={open}
+onClose={handleDialog}
+TransitionComponent={Transition}
+>
+<AppBar sx={{ background:'gray', position: 'absolute', zIndex:'1', height:'100vh'}}>
+  <Toolbar>
+    <Typography sx={{ ml: 2, flex: 1, textAlign:'center' }} variant="h3" component="div">
+    </Typography>
+  </Toolbar>
+</AppBar>
+<ListContainer>
+  <DialogUl>
+      <DialogBtn>
+        <DialogBtnTxt onClick={handleDialog}>
+          <DialogBtnOuter>
+            <DialogBtnInner>
+              <span>Home</span>
+            </DialogBtnInner>
+          </DialogBtnOuter>
+        </DialogBtnTxt>
+      </DialogBtn>
+      <DialogBtn>
+        <DialogBtnTxt onClick={handleDialog}>
+          <DialogBtnOuter>
+            <DialogBtnInner>
+              <span>About</span>
+            </DialogBtnInner>
+          </DialogBtnOuter>
+        </DialogBtnTxt>
+      </DialogBtn>
+      <DialogBtn>
+        <DialogBtnTxt onClick={handleDialog}>
+          <DialogBtnOuter>
+            <DialogBtnInner>
+              <span>Events</span>
+            </DialogBtnInner>
+          </DialogBtnOuter>
+        </DialogBtnTxt>
+      </DialogBtn>
+      <DialogBtn>
+        <DialogBtnTxt onClick={handleDialog}>
+          <DialogBtnOuter>
+            <DialogBtnInner>
+              <span>Contact</span>
+            </DialogBtnInner>
+          </DialogBtnOuter>
+        </DialogBtnTxt>
+      </DialogBtn>
+      {/* <SocialLi>
+        <SocialOuter>
+          <SocialInner>
+            <a href="https://www.facebook.com/bogiespub2/">
+              <img src={facebook} alt=""/></a>
+          </SocialInner>
+        </SocialOuter>
+      </SocialLi> */}
+  </DialogUl>
+  </ListContainer>
+</Dialog>
+</>
     );
 }
 
 export default Nav;
 
 // styled components //
+
+const Wrapper = styledCom.div`
+width:100vw;
+@media (max-width: 785px) {
+    width:100vw;
+    height:50px;
+    }
+`;
 
 const Navbar = styledCom.nav`
 display:block;
@@ -67,9 +154,13 @@ margin-right: 10vw;
 top: 0px;
 background-color: white;
 z-index: 300;
+@media (max-width: 785px) {
+    display:none;
+    }
 `;
 
 const NavLink = styledCom.div`
+margin-top:10px
 width:120px;
 text-align: center;
 &:active {
@@ -78,6 +169,9 @@ text-align: center;
 `;
 
 const NavLogo = styledCom.img`
+margin-top:10px;
+margin-left: auto;
+margin-right: auto;
 width: 250px;
 height: 70px;
 &:active {
@@ -85,3 +179,141 @@ height: 70px;
 }
 ;`
 
+const NavLogo2 = styledCom.img`
+display:none;
+@media (max-width: 785px) {
+    padding-top: -40px;
+    width: 200px;
+    height: 60px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    }
+;`
+
+
+const ListContainer = styledCom.div`
+background-color: gray;
+border: 0;
+width: 100%;
+margin: 0 auto;
+font-size: 16px;
+font-style: normal;
+font-weight: 500;
+color: #999999;
+-webkit-text-size-adjust: 100%;
+-webkit-tap-highlight-color: rgba(0,0,0,0);
+transition-property: transform;
+transition-duration: 500ms;
+transition-timing-function: cubic-bezier(0.400,0.000,0.200,1.000);
+z-index:200;
+`;
+
+const DialogUl = styledCom.ul`
+align-text:center;
+background-color:gray;
+position: relative;
+display: block;
+font-style: normal;
+font-weight: 500;
+color: #999999;
+padding-top:20vh;
+padding-bottom:20vh;
+height: 100vh;
+width: 100vw;
+list-style: none;
+align-items:center;
+justify-content:center;
+text-align: center;
+font-family: "Quicksand",sans-serif;
+font-size: 1em;
+overflow: hidden;
+overflow-wrap: break-word;
+box-sizing: border-box;
+transition-property: height;
+transition-duration: 300ms;
+transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+z-index:100;
+
+`;
+
+const DialogBtn = styledCom.li`
+display: list-item;
+text-align: -webkit-match-parent;
+min-width: 1px;
+font-size: 1em;
+box-sizing: border-box;
+align-items:center;
+justify-content:center;
+align-text:center;
+background-color:gray;
+`;
+
+const DialogBtnTxt = styledCom.button`
+opacity: 1;
+transform: translate(0, 0);
+font-size: 2em;
+background-color: gray;
+border: 0;
+`;
+
+// button background
+const DialogBtnOuter = styledCom.div`
+background-color:gray;
+padding-top: 0.5em;
+    padding-right: 0.5em;
+    padding-bottom: 0.5em;
+    padding-left: 0.5em;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    display: flex;
+    flex: 1 0 auto;
+    position: relative;
+    z-index: 2;
+    height: 100%;
+    border-radius: inherit;
+    transform: translate3d(0, 0, 0);
+    box-sizing: border-box;
+    `;
+
+    // button text
+    const DialogBtnInner = styledCom.div`
+    color:white;
+    flex-shrink: 1;
+    min-width: 1px;
+    max-width: 100%;
+    margin-left: 5px;
+    margin-top: 5px;
+    margin-right: 5px;
+    margin-bottom: 5px;
+    cursor: pointer;
+    &:hover {
+      transform: scale(1.1);
+      color:pink;
+    }
+    `;
+
+// icon
+const Menu = styledCom.img.attrs({
+    src:menu, alt:'menu'
+})`
+display:none;
+position:absolute; 
+margin-top:10px;
+right:3%;
+height:30px; 
+width:30px;
+&:hover {
+      transform: scale(1.1);
+    transition: 200ms ease-in;
+}
+&:active {
+    transform: scale(1)
+}
+@media (max-width: 785px) {
+    display:block;
+    }
+   
+`;
